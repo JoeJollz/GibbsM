5da20951dfbf38d623b143d02c4298b957e032d9
JoeJollz <joseph.jolley@student.manchester.ac.uk>
2025-04-14 14:59:37 +0100

    Fixing user input code...

--- GibbsCode.py
+++ GibbsCode.py
@@ -11,7 +11,11 @@ moles_of_species_in = {}
 C = 0
 H = 0
 O = 0
+total_moles = 0
 for _ in range(num_species_in):
+    add_c = 0
+    add_h = 0
+    add_o = 0
     if _ == 0:
         print('Please input using the following format: ')
         print('e.g. Ethanol -> c1h5o1')
@@ -22,27 +26,23 @@ for _ in range(num_species_in):
     moles = float(input(f"How many moles of species {_+1} is present? " ))
     moles_of_species_in[species_name] = moles
     
+    
     for Z in range(len(species_name)-1):
         if species_name[Z].isdigit():
             continue
         
         elif species_name[Z] == 'c' or 'C':
-            add_c = float(species_name[Z+1])
+            add_c = float(species_name[Z+1])*moles
         
         elif species_name[Z] == 'h' or 'H':
-            add_h = float(species_name[Z+1])
+            add_h = float(species_name[Z+1])*moles
         elif species_name[Z] == 'o' or 'O':
-            add_o = float(species_name[Z+1])
+            add_o = float(species_name[Z+1])*moles
         
         else:
             print("Chemical formula has invalid input. Exciting programme.")
             exit()
             
-    
-    add_c = float(species_name[1])*moles
-    add_h = float(species_name[3])*moles
-    add_o = float(species_name[5])*moles
-    
     C += add_c
     H += add_h
     O += add_o
